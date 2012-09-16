4475f927eb1164f7ddf283f9920c1857fb9020d6
erlon <erlonlon@gmail.com>
2012-09-15 21:42:07 -0300

    email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,12 +5,12 @@ class Contact < ActiveRecord::Base
    validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
    default_scope order: "created_at DESC"
 
-   after_create :deliver_notification
+   #after_create :deliver_notification
 
-   protected
-   def deliver_notification
+   #protected
+   #def deliver_notification
     
-    ContatoMailer.notification(self).deliver
+    #ContatoMailer.notification(self).deliver
 
-   end
+   #end
 end
