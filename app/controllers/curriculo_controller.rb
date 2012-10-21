e95248e47637d964c18c0401c28b4f9366f754a2
erlon <erlonlon@gmail.com>
2012-10-21 08:49:12 -0300

    curriculo atuaslização

--- app/controllers/curriculo_controller.rb
+++ app/controllers/curriculo_controller.rb
@@ -1,7 +1,17 @@
 class CurriculoController < ApplicationController
-  def index
+before_filter :load_resources
+  def index  	
 
-  	
+  end
+
+   def load_resources
+   @categories = Category.all
+    @category   = Category.find(params[:category_id]) if params[:category_id]
+    @galeries = Galery.all
+    
+     @category_galeries   = CategoryGalery.find(3)
+     @galeries_footer = CategoryGalery.find(3)
+     @galeries_clientes = CategoryGalery.find(5)
 
   end
 end
