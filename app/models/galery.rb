dad6cf2e93da1fd4c5c0d1f586537366d5af0fb5
erlon <erlonlon@gmail.com>
2012-12-08 21:28:05 -0300

    alteração

--- app/models/galery.rb
+++ app/models/galery.rb
@@ -3,7 +3,7 @@ class Galery < ActiveRecord::Base
   has_and_belongs_to_many :category_galeries
  
    validates_presence_of :title
-   has_attached_file :document, :styles => {:original =>"810x370>", :small => "1150x420>", :medium => "380x250>", :thumb => "100x100>" }
+   has_attached_file :document, :styles => {:original =>"810x370>", :small => "1150x420#", :medium => "380x250>", :thumb => "100x100>" }
    validates_attachment_presence :document
    validates_attachment_size :document, :less_than => 2.megabyte
    validates_attachment_content_type :document, :content_type => %w(image/png image/jpeg image/gif)
